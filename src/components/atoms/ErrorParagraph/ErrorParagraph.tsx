562ad020aabb6b3860f83fc9d811d1e68ede0ca3
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-08-07 16:28:40 +0200

    Add fade animation for ErrorParagraph components

--- src/components/atoms/ErrorParagraph/ErrorParagraph.tsx
+++ src/components/atoms/ErrorParagraph/ErrorParagraph.tsx
@@ -1,6 +1,17 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+    from {
+        opacity: 0;
+        transform: translateY(-15px);
+    } to {
+        opacity: 1;
+        transform: translateY(0);
+    }
+`;
 
 export const ErrorParagraph = styled.p`
   color: red;
   margin: 0;
+  animation: ${fadeIn} 0.5s ease-in-out forwards;
 `;
