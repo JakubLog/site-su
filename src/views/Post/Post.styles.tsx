e65dcba198c4b678d9d7abef1e7fee110a42412c
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-08-08 14:01:03 +0200

    Add post view (graphics) - Mobile & Desktop

--- src/views/Post/Post.styles.tsx
+++ src/views/Post/Post.styles.tsx
@@ -1,3 +1,77 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  margin-block: 50px;
+  @media (min-width: 1350px) {
+    padding-inline: 60px;
+    position: relative;
+    margin-block: 70px;
+  }
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  font-size: 27px;
+  padding: 15px 10px;
+  background-color: ${({ theme }) => theme.color.primary};
+  color: ${({ theme }) => theme.color.secondary};
+  @media (min-width: 1350px) {
+    text-align: left;
+    padding: 0;
+    padding-left: 25px;
+    padding-bottom: 10px;
+    padding-right: 60px;
+    width: fit-content;
+    border-bottom: 3px solid ${({ theme }) => theme.color.primary};
+    font-size: 40px;
+    color: ${({ theme }) => theme.color.primary};
+    background-color: unset;
+  }
+`;
+export const Content = styled.p`
+  font-size: 20px;
+  letter-spacing: 1px;
+  text-align: justify;
+  line-height: 1.3;
+  margin-block: 30px;
+  @media (min-width: 1350px) {
+    line-height: 1.5;
+    font-size: 25px;
+    margin-block: 70px;
+    padding-inline: 60px;
+  }
+`;
+export const Author = styled.h2`
+  font-size: 16px;
+  text-align: center;
+  padding-top: 10px;
+  opacity: 0.8;
+  font-weight: 400;
+  letter-spacing: 1px;
+  border-top: 3px solid ${({ theme }) => theme.color.primary};
+  @media (min-width: 1350px) {
+    text-align: right;
+  }
+`;
+
+export const ImageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(95, 92, 112, 0.6);
+  @media (min-width: 1350px) {
+    width: fit-content;
+    opacity: 0.3;
+    position: absolute;
+    right: 75px;
+    top: 35px;
+  }
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  max-width: 600px;
+  @media (min-width: 1350px) {
+    max-height: 350px;
+  }
+`;
--- src/views/Post/Post.tsx
+++ src/views/Post/Post.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Wrapper } from './Post.styles';
+import { Wrapper, Title, Content, Author, Image, ImageWrapper } from './Post.styles';
 import { useQuery } from 'graphql-hooks';
 import Loading from 'components/molecules/Loading/Loading';
 import { useError } from 'hooks/useError';
@@ -8,14 +8,16 @@ import { useError } from 'hooks/useError';
 const Post = (): JSX.Element => {
   // Global States and Variables
   const { id: postId } = useParams<Partial<{ id: string }>>();
-  const [post, setPost] = useState<{ title: string; description: string }>({
+  const [post, setPost] = useState<{ title: string; description: string; createdBy: { name: string }; src: { url: string } }>({
     title: 'Error 404',
-    description: "This post isn't exist. We can't find this ID in our DataBase. If it's our mistake, please contact with our support!"
+    description: "This post isn't exist. We can't find this ID in our DataBase. If it's our mistake, please contact with our support!",
+    createdBy: { name: 'Administracja serwisu' },
+    src: { url: 'https://www.wwf.pl/sites/default/files/inline-images/25311985_l.jpg' }
   });
   const { dispatchError } = useError();
 
   // GraphQL query for Post with required id
-  const query = `{posts(where:{id:"${postId}"}){description, title}}`;
+  const query = `{posts(where:{id:"${postId}"}){description,title,createdBy{name},src{url}}}`;
   const { data, loading, error } = useQuery(query);
 
   useEffect(() => {
@@ -30,7 +32,22 @@ const Post = (): JSX.Element => {
     }
   }, [loading]);
 
-  return <Wrapper>{loading ? <Loading /> : post.title}</Wrapper>;
+  return (
+    <Wrapper>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <ImageWrapper>
+            <Image src={post.src.url} alt={post.title} />
+          </ImageWrapper>
+          <Title>{post.title}</Title>
+          <Content>{post.description}</Content>
+          <Author>{`Samorząd Uczniowski - ${post.createdBy.name}`}</Author>
+        </>
+      )}
+    </Wrapper>
+  );
 };
 
 export default Post;
