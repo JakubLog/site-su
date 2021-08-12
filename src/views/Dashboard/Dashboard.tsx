071d21bdb769be78e804c50af3db406cc7026651
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-08-12 12:29:57 +0200

    Delete console.log from useEffect

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -28,7 +28,6 @@ const Dashboard = (): JSX.Element => {
         })
       );
     setLoadingState(false);
-    console.log(user);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
