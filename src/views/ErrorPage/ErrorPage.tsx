5ae8ea423dfae03d0a958193fc9f7a18a3dde899
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-08-24 13:17:42 +0200

    Exclude styles from ErrorPage to ErrorPage.styles

--- src/views/ErrorPage/ErrorPage.tsx
+++ src/views/ErrorPage/ErrorPage.tsx
@@ -1,40 +1,6 @@
-import { Bold } from 'components/atoms/Bold/Bold';
 import React from 'react';
-import styled from 'styled-components';
-
-export const Wrapper = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const Block = styled.div`
-  width: 100%;
-  max-width: 600px;
-  height: 400px;
-  background-color: ${({ theme }) => theme.color.primary};
-  color: ${({ theme }) => theme.color.secondary};
-  padding: 20px;
-`;
-
-export const Title = styled.h1`
-  margin: 0;
-  padding-bottom: 10px;
-  margin-bottom: 10px;
-  border-bottom: 3px solid ${({ theme }) => theme.color.secondary};
-`;
-
-export const Content = styled.div`
-  padding: 10px;
-  background-color: ${({ theme }) => theme.color.secondary};
-  height: 84%;
-  & > * {
-    margin: 0;
-    color: ${({ theme }) => theme.color.primary};
-    letter-spacing: 1px;
-  }
-`;
+import { Bold } from 'components/atoms/Bold/Bold';
+import { Wrapper, Block, Title, Content } from './ErrorPage.styles';
 
 const ErrorPage = (): JSX.Element => {
   return (
