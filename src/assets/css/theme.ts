41c25ecd5b316fddf5a53e23259aca4344392421
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-08-24 12:24:42 +0200

    Remove useless variables from theme

--- src/assets/css/theme.ts
+++ src/assets/css/theme.ts
@@ -1,31 +1,10 @@
-const base = {
-  fontSize: {
-    xs: '',
-    sm: '',
-    md: '',
-    lg: '',
-    xl: ''
-  },
-  margin: {
-    sm: '',
-    md: '',
-    lg: ''
-  },
-  padding: {
-    sm: '',
-    md: '',
-    lg: ''
-  }
-};
-
 export const light = {
   color: {
     primary: 'hsl(249, 10%, 40%)',
     secondary: 'hsl(0, 0%, 95%)',
     primaryDarken: 'hsl(249, 10%, 20%)',
     secondaryDarken: 'hsl(0, 0%, 85%)'
-  },
-  ...base
+  }
 };
 
 export const dark = {
@@ -34,6 +13,5 @@ export const dark = {
     secondary: 'hsl(210, 30%, 20%)',
     primaryDarken: 'hsl(80, 60%, 40%)',
     secondaryDarken: 'hsl(210, 30%, 10%)'
-  },
-  ...base
+  }
 };
