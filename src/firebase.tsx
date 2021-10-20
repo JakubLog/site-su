d9749482e08e501490c606da53dac236c21f097c
Jakub Michał Fedoszczak <77537823+JakubLog@users.noreply.github.com>
2021-10-20 16:45:01 +0200

    Update firebase.tsx

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -12,16 +12,6 @@ const app = firebase.initializeApp({
   appId: '1:248071799426:web:534c636cc899ae3dfe2a9b'
 });
 
-// Production
-// const app = firebase.initializeApp({
-//   apiKey: 'AIzaSyCGy4ArWMpmGZclBewQo6OjiUDrBOm7yrk',
-//   authDomain: 'site-su-prod.firebaseapp.com',
-//   projectId: 'site-su-prod',
-//   storageBucket: 'site-su-prod.appspot.com',
-//   messagingSenderId: '339580091725',
-//   appId: '1:339580091725:web:738fe0a998e79ea2eb4b04'
-// });
-
 // Methods from firebase
 export const auth = app.auth();
 export const db = app.firestore();
